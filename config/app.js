ea2f38acc7bbbb16f1d50c3c828c0f510792d7af
Caique <caique.nunes@everis.com>
2020-09-04 11:22:05 -0300

    feat: implemented conversation routes

--- config/app.js
+++ config/app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 const sessionRoutes = require('../src/routes/sessions')
+const conversationRoutes = require('../src/routes/conversations')
+
+app.use(express.json())
 
 app.use('/session', sessionRoutes)
+app.use('/conversation', conversationRoutes)
 
 module.exports = app
